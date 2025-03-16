996cf15144110c13d2b8a8312ddc0418fb8bd1d1
Federico <steidlmartinez@gmail.com>
2025-03-16 16:32:38 +0100

    update premake5 shenanigans

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,39 @@
 ---@diagnostic disable: undefined-global, undefined-field
-VULKAN_SDK = os.getenv("VULKAN_SDK")
+
+include "River"
 
 workspace("Cnake")
     configurations({ "Debug", "Release" })
     platforms({ "Win64" })
     location("build")
 
-    project("River")
-        kind("StaticLib")
-        links{ "glfw3", "vulkan-1" }
-        language("C++")
-        cppdialect("C++20")
-        targetdir("bin/%{cfg.buildcfg}/%{prj.name}")
-        includedirs({ "./river/include/", "./river/vendor/glfw-3.4-win64/include/", "%{VULKAN_SDK}/Include/" })
-        syslibdirs({ "%{VULKAN_SDK}/Lib/", "./river/vendor/glfw-3.4-win64/lib-vc2022/" })
-        files({ "river/**.h", "river/**.c", "river/**.hpp", "river/**.cpp" })
-
     project("Cnake")
         kind("WindowedApp")
         links("River")
         language("C++")
         cppdialect("C++20")
-        targetdir("bin/%{cfg.buildcfg}/%{prj.name}")
+        targetdir("bin/%{cfg.buildcfg}")
         includedirs({ "./river/include/", "./river/vendor/glfw-3.4-win64/include/", "%{VULKAN_SDK}/Include/" })
         syslibdirs({ "%{VULKAN_SDK}/Lib/", "./river/vendor/glfw-3.4-win64/lib-vc2022/" })
         files({ "src/**.h", "src/**.c", "src/**.hpp", "src/**.cpp" })
 
-    filter("configurations:Debug")
-        defines("DEBUG")
-        symbols("On")
-        runtime("Debug")
-        ignoredefaultlibraries({ "MSVCRT" })
+filter("configurations:Debug")
+    -- defines{"DEBUG", "_DEBUG"}
+    -- staticruntime("off")
+    runtime("Debug")
+    symbols("On")
+    ignoredefaultlibraries({ "MSVCRT" })
+    targetsuffix "_d"
 
-    filter("configurations:Release")
-        defines("NDEBUG")
-        runtime("Release")
-        optimize("On")
+filter("configurations:Release")
+    -- staticruntime("off")
+    runtime("Release")
+    -- symbols("Off")
+    optimize("On")
 
-    filter("platforms:Win64")
-        system("Windows")
-        architecture("x86_64")
+filter("platforms:Win64")
+    system("Windows")
+    architecture("x86_64")
 
     newaction({
         trigger = "clean",
