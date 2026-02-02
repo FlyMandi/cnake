b2b9a036cb98153de5a9f6141a75dc65299e9c65
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:45:14 +0100

    refactor

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,4 @@
-#include "h/game.h"
-//TODO: convert these into github submodules, then include back
-#include "h/engine.h"
-#include "h/window.h"
+#include <river>
 
 void initGame(){
     std::ios_base::sync_with_stdio(false);
