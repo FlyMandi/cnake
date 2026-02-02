b1a2b02b67fcb81578d9d3855dbf9bfc51b38dcc
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:45:15 +0100

    specify entry point

--- assemble.ps1
+++ assemble.ps1
@@ -5,7 +5,7 @@ if(-Not (Test-Path ".\bin\")){
     &mkdir ".\bin\"
 }
 Push-Location ".\build\"
-$masmargs = '..\src\snek.asm', '/Fo .\snek.obj', '/Fe ..\bin\snek.exe'
+$masmargs = '..\src\snek.asm', '/Fo .\snek.obj', '/Fe ..\bin\snek.exe', '/link /entry:MainEntry'
 
 Invoke-Expression "ml64 $masmargs" -ErrorAction Break
 Pop-Location
