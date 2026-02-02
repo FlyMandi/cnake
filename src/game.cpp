372996e939bb425733a294c47e3819db524804bf
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:45:14 +0100

    cleanup

--- src/game.cpp
+++ src/game.cpp
@@ -3,21 +3,16 @@
 
 #include "game.h"
 
-#include <iostream>
-
 void initGame(){
     using namespace river;
 
     appName = "cnake";
     appVersion = "0.0.1";
 
-    //TODO: add error handling here
     window::WIDTH = 1920;
     window::HEIGHT = 1080;
 
     getProjectRoot(appName);
-
-    std::ios_base::sync_with_stdio(false);
 }
 
 void gameLoop(){
