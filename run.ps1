9b68337bd8d681dc73ff0291016cd5e7d9415c1a
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:45:15 +0100

    unite & fix run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,37 @@
-Invoke-Expression ".\bin\snek.exe"
+param(
+    $config = "debug"
+)
+
+if(-Not (Test-Path ".\build\"))
+{
+    &mkdir ".\build\"
+}
+if(-Not (Test-Path ".\bin\"))
+{
+    &mkdir ".\bin\"
+}
+
+if($config -eq "release")
+{
+    $masmargs = '..\src\snek.asm',
+                '/Fo .\snek.obj',
+                '/Fe ..\bin\snek.exe',
+                '/link /entry:MainEntry /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64"'
+}
+elseIf($config -eq "debug")
+{
+    $masmargs = '..\src\snek.asm',
+                '/Fo .\snek.obj',
+                '/Fe ..\bin\snek.exe',
+                '/DEBUG',
+                '/link /entry:MainEntry /DEBUG:full /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64"'
+}
+
+Push-Location ".\build\"
+Invoke-Expression "ml64 $masmargs" -ErrorAction Break
+Pop-Location
+
+if($LASTEXITCODE -eq 0)
+{
+    Invoke-Expression ".\bin\snek.exe"
+}
